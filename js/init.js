bc3afbc9ed5491223e4a3d8fb011967db7b614de
Roberto12541 <robertomiranda1254@gmail.com>
2022-11-29 19:11:44 -0600

    Firebase

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyD1-ZTv9IowlHci1p1FcyQLRx82ODu5axo",
+  authDomain: "proyecto43mmra-ad120.firebaseapp.com",
+  projectId: "proyecto43mmra-ad120",
+  storageBucket: "proyecto43mmra-ad120.appspot.com",
+  messagingSenderId: "391513559129",
+  appId: "1:391513559129:web:d6b82bd64c32d681416148",
+  measurementId: "G-D8R7F5S70R"
 });
